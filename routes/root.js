fb7131a09b5f50203e2609f73e8bf6e95e8f44d0
Syed Khalid <magmastar167@gmail.com>
2023-11-04 17:55:07 +0300

    🤥

--- routes/root.js
+++ routes/root.js
@@ -1,9 +1,10 @@
-const express = require('express')
-const router = express.Router()
-const path = require('path')
+const express = require("express");
+const router = express.Router();
+const path = require("path");
 
-router.get('^/$|/index(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'views', 'index.html'))
-})
+router.get("^/$|/index(.html)?", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "views", "index.html"));
+});
 
-module.exports = router
+module.exports = router;
+
--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const { logger } = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
 const PORT = process.env.PORT || 3500;
+
+app.use(logger);
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+app.use(cookieParser());
 
 app.use("/", express.static(path.join(__dirname, "public")));
 
@@ -18,4 +31,6 @@ app.all("*", (req, res) => {
   }
 });
 
-app.listen(PORT, () => console.log(`Server runing on port ${PORT}`));
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
